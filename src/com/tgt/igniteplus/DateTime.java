4c2d77d44f50b79d84273038c23d073631159369
somya1209 <somya123024@gmail.com>
2020-03-22 18:03:44 +0530

    current date time

---
+++ src/com/tgt/igniteplus/DateTime.java
@@ -0,0 +1,8 @@
+package com.tgt.igniteplus;
+import  java.util.Date;
+public class DateTime {
+    public static void main(String[]args){
+        Date date=new Date();
+        System.out.println(date);
+    }
+}
