260eee4c182b3910b53d1daa9e71216549c96d62
somya1209 <somya123024@gmail.com>
2020-03-23 02:10:18 +0530

    duplicate char in str

---
+++ src/com/tgt/igniteplus/DuplicateChar.java
@@ -0,0 +1,41 @@
+package com.tgt.igniteplus;
+
+/*
+hashing
+if first occurrence:inc
+if second occurrence:inc print
+ */
+import java.util.Scanner;
+
+public class DuplicateChar {
+    public static void main(String[]args) {
+        String str;
+
+        int []countChar=new int[26];
+        Scanner in = new Scanner(System.in);
+        System.out.println("String  : ");
+        str = in.nextLine();
+        char[] s=str.toCharArray();
+
+        for (int i = 0; i <s.length ; i++) {
+            int count=countChar[s[i]-'a'];
+
+            if(count==0)
+                countChar[s[i]-'a']++;
+            else if(count==1) {
+                System.out.println(s[i]);
+                //will not be printed on next encounter
+                countChar[str.charAt(i)-'a']++;
+            }
+
+        }
+
+    }
+}
+/*
+String  :
+abcahjdshc
+a
+h
+c
+ */
