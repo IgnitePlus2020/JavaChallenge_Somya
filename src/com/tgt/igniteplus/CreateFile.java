dcf87966471df94031248cdafba7b84c368b4070
somya1209 <somya123024@gmail.com>
2020-03-26 12:58:51 +0530

    file creation

---
+++ src/com/tgt/igniteplus/CreateFile.java
@@ -0,0 +1,17 @@
+package com.tgt.igniteplus;
+
+import java.io.File;
+import java.io.IOException;
+
+public class CreateFile {
+    public  static  void main(String[] args){
+        try{
+            File file=new File("C:\\Users\\SOMYA\\IdeaProjects\\JavaChallenge_Somya\\myfile.txt");
+            if (file.createNewFile())System.out.println("Success!");
+            else  System.out.println("Error file already exists.");
+        }
+        catch (IOException ioe){
+            ioe.printStackTrace();
+        }
+    }
+}
