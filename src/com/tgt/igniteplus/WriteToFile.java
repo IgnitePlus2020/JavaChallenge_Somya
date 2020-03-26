100a750a054c653b176b7cc1d72ea7cf527edfde
somya1209 <somya123024@gmail.com>
2020-03-26 13:17:14 +0530

    writing to a file

---
+++ src/com/tgt/igniteplus/WriteToFile.java
@@ -0,0 +1,21 @@
+package com.tgt.igniteplus;
+
+import java.io.*;
+import java.io.File;
+import java.io.IOException;
+public class WriteToFile {
+
+        public  static  void main(String[] args){
+            try{
+                String fileContent="writing to this file";
+                FileOutputStream outputStream=new FileOutputStream("C:\\Users\\SOMYA\\IdeaProjects\\JavaChallenge_Somya\\myfile.txt");
+               byte[] strToBytes=fileContent.getBytes();
+               outputStream.write(strToBytes);
+               outputStream.close();
+            }
+            catch (IOException ioe){
+                ioe.printStackTrace();
+
+        }
+    }
+}
